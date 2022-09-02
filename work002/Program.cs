43f153e7a89884e86501bd51ca90375db108e74f
Maxim <maxiprav88@gmail.com>
2022-09-02 16:04:08 +0300

    new fale

---
+++ work002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+
+if (numberText.Length > 2)
+{
+    Console.WriteLine("Третья цифра -> " +numberText[2]);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ work003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введи цифру, обозначающую день недели: ");
+int dyaNumber = Convert.ToInt32(Console.ReadLine());
+
+void DayTheWeek (int dayNumber) {
+  if (dyaNumber == 6 || dyaNumber == 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (dyaNumber < 1 || dyaNumber > 7) {
+    Console.WriteLine("это вообще не день недели");
+  }
+  else Console.WriteLine("(этот день не выходной) -> нет");
+}
+
+DayTheWeek(dyaNumber);
